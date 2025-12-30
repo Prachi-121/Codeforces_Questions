43fa22b1afba029a13771a10eb01f96a1d2f8ab4
Prachi verma <81434390+Prachi-121@users.noreply.github.com>
2025-12-30 17:44:34 +0530

    Update 1_1296.cpp

--- 1_1296.cpp
+++ 1_1296.cpp
@@ -1,1 +1,26 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+  int t;
+  cin>>t;
+  while(t--){ 
+    int len;
+    cin>>len;
+    int even=0,odd=0;
+    for(int j=0;j<len;j++){
+        int x;
+        cin>>x;
+        if(x%2!=0) odd++;
+        else even++;
+    }
+    if (odd == 0) {
+            cout << "NO\n";
+        } else if (even == 0 && len % 2 == 0) {
+            cout << "NO\n";
+        } else {
+            cout << "YES\n";
+        }
+  }
+    return 0;
+}
